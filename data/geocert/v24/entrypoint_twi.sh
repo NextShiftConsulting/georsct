ddf67e03da1f21ec733826b84e71ea8f9178b876
ACAMAR7\marti <rmartin@iwebconsultant.com>
2026-05-25 15:10:49 -0500

    fix(twi): make --county-dir explicit in entrypoint

--- data/geocert/v24/entrypoint_twi.sh
+++ data/geocert/v24/entrypoint_twi.sh
@@ -5,4 +5,5 @@ pip install -q requests pyarrow
 
 python3 -u /opt/ml/processing/input/code/run_twi.py \
     --data-dir /opt/ml/processing/input/data \
+    --county-dir /opt/ml/processing/input/county \
     --output-dir /opt/ml/processing/output
